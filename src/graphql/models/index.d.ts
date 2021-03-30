a5aef103c2f7b5e12b7a5841bc41bd54fc78ea43
Joshua Newbins <jnewbins@gmail.com>
2021-03-30 15:40:32 -0500

    adding backend

---
+++ src/graphql/models/index.d.ts
@@ -0,0 +1,13 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+
+
+
+
+export declare class Note {
+  readonly id: string;
+  readonly name: string;
+  readonly description?: string;
+  constructor(init: ModelInit<Note>);
+  static copyOf(source: Note, mutator: (draft: MutableModel<Note>) => MutableModel<Note> | void): Note;
+}
